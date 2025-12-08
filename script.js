2f914f1f889864704807c53d20fc8bc1265d1379
Mimosa Dimension <mimosadimension@gmail.com>
2025-12-09 02:26:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 */
 
 // ---- Constants: keep these exactly as required ----
-const LOGICAL_W = 1000;
-const LOGICAL_H = 800;
+const LOGICAL_W = 700;
+const LOGICAL_H = 500;
 
 // ---- DOM ----
 const paintCanvas = document.getElementById('paintCanvas');
